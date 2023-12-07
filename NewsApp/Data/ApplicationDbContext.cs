a6944ade45973c77d457017df0b9956116524b98
zzzzDenzzzz <12den24@gmail.com>
2023-12-07 13:25:41 +0300

    fix

--- NewsApp/Data/ApplicationDbContext.cs
+++ NewsApp/Data/ApplicationDbContext.cs
@@ -4,12 +4,7 @@ using NewsApp.Models.Entities;
 
 namespace NewsApp.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<News> News => Set<News>();
 }
